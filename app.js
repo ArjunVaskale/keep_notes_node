069fb3b66c960a5c985a0e4eda9d0c896173270c
dudu <dudu@Arjuns-Air.bbrouter>
2022-12-22 21:50:09 +0530

    insert functionlity working

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 const express = require("express");
-
+const bodyParser = require('body-parser');
 const app = express();
-
 const conn = require('./config/database');
+const cors = require('cors')
+
+app.use(cors())
+
+// app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(express.json())
+
 
 const Notes = require('./model/notes');
 
@@ -11,8 +18,17 @@ conn();
 app.get('/' , async (req , res)=>{
         Notes.find({} ,{note : 1 , _id : 0} , (err , data )=>{
             if(err) throw err;
-            res.json(data) 
+            res.json(data)
         })
     });
+
+app.post('/' , (req , res)=>{
+    console.log(req.body);
+    Notes.create({note : req.body.item} , (err , data)=>{
+        if(err) return err ;
+        console.log(data);
+        })
+    res.send('inserted!')
+})
 
 module.exports = app ;
