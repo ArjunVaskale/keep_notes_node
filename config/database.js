152e57c37be3d6a359441f242acdc0ecd4fb2827
dudu <dudu@Arjuns-Air.bbrouter>
2023-01-07 01:59:10 +0530

    add mongodb url

--- config/database.js
+++ config/database.js
@@ -1,13 +1,39 @@
-const mongoose = require('mongoose');
+// const mongoose = require('mongoose');
+// const URL = "mongodb://localhost:27017/keet_notes" ;
+// mongoose.set('strictQuery', true);
 
-const URL = "mongodb://localhost:27017/keet_notes" ;
+// const conn = () =>{
+//     mongoose.connect(URL , { useNewUrlParser: true, useUnifiedTopology: true })
+//     .then(()=>{console.log('successfully Connected!!!')})
+//     .catch((err)=>{console.log(err)});
+// } 
+// module.exports = conn 
+
+
+
+const mongoose = require('mongoose');
+const {ServerApiVersion } = require('mongodb');
+
+const URL = "mongodb+srv://root:mongopass@cluster0.42gwgmd.mongodb.net/?retryWrites=true&w=majority" ;
 
 mongoose.set('strictQuery', true);
 
 const conn = () =>{
-    mongoose.connect(URL , { useNewUrlParser: true, useUnifiedTopology: true })
+    mongoose.connect(URL, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 })
     .then(()=>{console.log('successfully Connected!!!')})
     .catch((err)=>{console.log(err)});
 } 
 
 module.exports = conn 
+
+
+
+
+
+// const uri = "mongodb+srv://root:mongopass@cluster0.42gwgmd.mongodb.net/?retryWrites=true&w=majority";
+// const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+// const conn = () => client.connect(err => {
+//   const collection = client.db("test").collection("devices");
+//   // perform actions on the collection object
+//   client.close();
+// });
